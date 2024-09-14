9da5cbe11570ad899725d8cae611e48768226adc
CIM <cim@kortekh.ru>
2024-09-14 20:32:25 +0300

    Test easy connect with cluster

--- server.go
+++ server.go
@@ -1,36 +1,86 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
+	"encoding/binary"
 	"fmt"
-	"log"
-	"math/rand"
 	"net"
-	"strings"
-	"time"
 )
 
-func main() {
-	ln, err := net.Listen("tcp", ":8000")
+type Packer struct {
+	F1 float64
+	F2 float64
+}
+
+// Функция для разборки структуры из байт
+func unpackFromBytes(data []byte) (Packer, error) {
+	var p Packer
+	buf := bytes.NewReader(data)
+	err := binary.Read(buf, binary.BigEndian, &p)
 	if err != nil {
-		log.Fatal(err)
+		return p, err
 	}
-	fmt.Println("Listening on port 8000")
+	return p, nil
+}
+
+func packData(p Packer) ([]byte, error) {
+	// Создаем буфер для хранения байт
+	buf := new(bytes.Buffer)
+
+	// Пишем данные структуры в буфер
+	err := binary.Write(buf, binary.BigEndian, p)
+	if err != nil {
+		return nil, err
+	}
+
+	// Возвращаем срез байт
+	return buf.Bytes(), nil
+}
+
+// Функция для получения и распаковки пакета через TCP
+func receivePack(conn net.Conn) (Packer, error) {
+	data := make([]byte, 16) // float64 занимает 8 байт, 2 float64 = 16 байт
+	_, err := conn.Read(data)
+	if err != nil {
+		return Packer{}, err
+	}
+	return unpackFromBytes(data)
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server is listening...")
+
 	for {
-		rand.Seed(time.Now().UnixNano())
-		conn, err := ln.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println("Error accepting connection:", err)
+			continue
 		}
-		message, err := bufio.NewReader(conn).ReadString('\n')
+		defer conn.Close()
+
+		p, err := receivePack(conn)
 		if err != nil {
-			//log.Warning(err)
+			fmt.Println("Error receiving Packer:", err)
+			continue
 		}
-		if len(message) > 0 {
-			fmt.Print("Message Received:", string(message))
-			newmessage := strings.ToUpper(message)
-			conn.Write([]byte(newmessage + string(rand.Intn(100)) + "\n"))
-			//conn.Close()
+
+		// Выводим пакет в консоль в hex формате
+		fmt.Printf("Received Packer: %+v\n", p)
+		buf, err := packData(p)
+		if err == nil {
+			_, err := conn.Write(buf)
+			if err != nil {
+				fmt.Println("Error receiving Packer:", err)
+				continue
+			}
 		}
+
 	}
 }
