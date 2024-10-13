bfb6253b83d375b125e28a758fd043729ed14b46
CIM <cim@kortekh.ru>
2024-10-13 20:43:02 +0300

    Add gorutines, and reply. App become async and ready for multiple connections. Limit seted to 100 connections.

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net"
@@ -10,90 +9,58 @@ import (
 	"time"
 )
 
-type Packer struct {
-	F1 float64
-	F2 float64
+type Data struct {
+	CMD    uint8
+	Keys   []string
+	Values []float64
 }
 
-// Функция для разборки структуры из байт
-func unpackFromBytes(data []byte) (Packer, error) {
-	var p Packer
-	buf := bytes.NewReader(data)
-	err := binary.Read(buf, binary.BigEndian, &p)
+func (d Data) print(buf []byte, lend int) {
+	err := json.Unmarshal(buf[:lend], &d)
 	if err != nil {
-		return p, err
+		fmt.Println(err)
+		return
 	}
-	return p, nil
-}
-
-func packData(p Packer) ([]byte, error) {
-	tmp_p := p
-	tmp_p.F1 += 1
-	tmp_p.F2 += 2.22222
-	// Создаем буфер для хранения байт
-	buf := new(bytes.Buffer)
-
-	// Пишем данные структуры в буфер
-	err := binary.Write(buf, binary.BigEndian, tmp_p)
-	if err != nil {
-		return nil, err
+	fmt.Println("CMD:", d.CMD)
+	fmt.Print("Keys: ")
+	for _, v := range d.Keys {
+		fmt.Print(string(v) + ",")
 	}
-
-	// Возвращаем срез байт
-	return buf.Bytes(), nil
-}
-
-// Функция для получения и распаковки пакета через TCP
-func receivePack(conn net.Conn) (Packer, error) {
-	data := make([]byte, 16) // float64 занимает 8 байт, 2 float64 = 16 байт
-	_, err := conn.Read(data)
-	if err != nil {
-		if err == io.EOF {
-			fmt.Println("Client closed connection")
-		}
-		return Packer{}, err
+	fmt.Println("")
+	fmt.Print("Values: ")
+	for _, v := range d.Values {
+		s := fmt.Sprintf("%f,", v)
+		fmt.Print(s)
 	}
-	return unpackFromBytes(data)
+	fmt.Println("")
 }
 
 // Обработка каждого подключения в отдельной горутине
-func handleConnection(conn net.Conn, wg *sync.WaitGroup) {
+func handleConnections(conn net.Conn, wg *sync.WaitGroup, dd *Data) {
 	defer wg.Done()
-
 	// Устанавливаем таймаут на чтение и запись
 	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 	conn.SetWriteDeadline(time.Now().Add(5 * time.Second))
 
 	for {
-		p, err := receivePack(conn)
+		header := make([]byte, 1024)
+		lend, err := conn.Read(header)
 		if err != nil {
-			// Если клиент закрыл соединение
 			if err == io.EOF {
 				break
 			}
-			fmt.Println("Error receiving Packer:", err)
+			fmt.Println("Error receiving Packet:", err)
 			break
 		}
-
-		// Выводим полученные данные от клиента
-		fmt.Printf("Received Packer from client: %+v\n", p)
-
+		dd.print(header[:lend], lend)
+		//printData(header, lend)
 		// Пакуем данные и отправляем обратно
-		buf, err := packData(p)
-		if err != nil {
-			fmt.Println("Error packing data:", err)
-			break
-		}
-
-		_, err = conn.Write(buf)
+		buf := "OK"
+		_, err = conn.Write([]byte(buf))
 		if err != nil {
 			fmt.Println("Error writing response:", err)
 			break
 		}
-
-		// Выводим данные, которые были отправлены обратно клиенту
-		fmt.Printf("Sent back to client: %+v\n", p)
-
 		// Сбрасываем таймауты после каждой успешной операции
 		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 		conn.SetWriteDeadline(time.Now().Add(5 * time.Second))
@@ -105,13 +72,13 @@ func handleConnection(conn net.Conn, wg *sync.WaitGroup) {
 }
 
 func main() {
+	var d Data
 	listener, err := net.Listen("tcp", ":8000")
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 		return
 	}
 	defer listener.Close()
-
 	fmt.Println("Server is listening...")
 
 	var wg sync.WaitGroup
@@ -130,7 +97,7 @@ func main() {
 
 		go func(conn net.Conn) {
 			defer func() { <-sem }()
-			handleConnection(conn, &wg)
+			handleConnections(conn, &wg, &d)
 		}(conn)
 	}
 
